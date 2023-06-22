56246251222a0dce69eb9c01180904feddae143a
Zakaria96 <zakarialavoro@gmail.com>
2023-06-22 15:52:37 +0200

    struttura codice e axios

--- back_end/index.php
+++ back_end/index.php
@@ -1,3 +1,8 @@
 <?php
-echo "hello word";
+header("Access-Control-Allow-Origin: http://http://localhost:5173/");
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+$studentStr = file_get_contents('data.json');
+
+echo $studentStr;
 ?>
---
+++ front_end/src/components/MainApp.vue
@@ -0,0 +1,54 @@
+<script>
+import axios from 'axios'
+export default {
+  name: "MainApp",
+  data() {
+    return {
+      elements: [],
+      newElement: "",
+    }
+
+  },
+  methods: {
+    onSubmit() {
+      axios.post('http://localhost/esercizio_php-todo-list-json_22_06_2023/php-todo-list-json/back_end/newElement.php')
+        .then(response => {
+          this.newElement = response.data;
+        });
+
+    }
+
+  },
+  mounted() {
+
+    axios.get('http://localhost/esercizio_php-todo-list-json_22_06_2023/php-todo-list-json/back_end/index.php')
+      .then(response => {
+        this.elements = response.data;
+      });
+  }
+}
+
+</script>
+
+
+
+<template>
+  <div class="container_fluid bg-dark">
+    <div class="container text-center">
+      <h1 style="color: #6d757d ;">Todo List</h1>
+      <div class="card bg ">
+        <div class="card-body">
+          <h3>{{ newElement }}</h3>
+        </div>
+        <div class="card-footer">
+          <form @submit.prevent="onSubmit">
+            <input type="text" name="element" placeholder="Inserisci nuovo elemento" v-model="newElement">
+            <input type="submit" value="Create New Element">
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
