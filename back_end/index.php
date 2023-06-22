1655c6c4c0c7f0719bf82538ad949e58b84bd6f4
Zakaria96 <zakarialavoro@gmail.com>
2023-06-22 16:41:24 +0200

    ticket

--- back_end/index.php
+++ back_end/index.php
@@ -1,8 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: http://http://localhost:5173/");
+header("Access-Control-Allow-Origin: http://localhost:5174");
 header("Access-Control-Allow-Headers: X-Requested-With");
 
-$studentStr = file_get_contents('data.json');
+$elementStr = file_get_contents('store.json');
 
-echo $studentStr;
+echo $elementStr;
 ?>
--- back_end/newElement.php
+++ back_end/newElement.php
@@ -1,9 +1,18 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: http://localhost:5174");
 header("Access-Control-Allow-Headers: X-Requested-With");
 
-header('Content-Type: application/json');
+// header('Content-Type: application/json');
 
-$dataStr = file_get_contents()
+$dataStr = file_get_contents('store.json')
+$newElement = $_POST;
+$data = json_decode($dataStr);
+
+$data[]= $newElement
+$encData = json_encode($data);
+
+file_put_contents('store.json', $encData);
+
+echo $encData;
 ?>
